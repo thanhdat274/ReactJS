53b23d1403e71634c6af65509b704b76321a2d77
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-07-11 17:39:04 +0700

    update

--- BH4/src/App.tsx
+++ BH4/src/App.tsx
@@ -1,45 +1,28 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import styled from 'styled-components';
+import { Route, Routes } from 'react-router-dom';
+import Home from './client/Home';
+import Product from './client/Products/product.';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Container>
+      <Routes>
+        <Route path='/' element={<Home/>}>
+          <Route path='/products' element={<Product/>}/>
+        </Route>
+      </Routes>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  text-align: center;
+`
+
 
 export default App
---
+++ BH4/src/client/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        trang chủ
+    </div>
+  )
+}
+
+export default Home
