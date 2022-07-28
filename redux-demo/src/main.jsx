cc4c7d2a43454414fcc731cd5281c108ca973cb0
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-07-29 00:24:46 +0700

    update code redux

--- redux-demo/src/main.jsx
+++ redux-demo/src/main.jsx
@@ -4,9 +4,10 @@ import App from './App'
 import './index.css'
 
 import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
+</Provider>
 )
